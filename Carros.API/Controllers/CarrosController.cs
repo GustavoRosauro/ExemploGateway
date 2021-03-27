97d93deb27b4b5cb61fed917f1060a38cb37694b
GustavoRosauro <gustavosrosauro@gmail.com>
2021-03-27 11:52:51 -0300

    'gateway'

---
+++ Carros.API/Controllers/CarrosController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Carros.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarrosController : ControllerBase
+    {
+        [HttpGet]
+        public string[] RetornarCarros() => new string[]
+            {
+                "BMW",
+                "JETTA",
+                "FUSION",
+                "ONIX"
+            };
+    }
+}
---
+++ Carros.Cliente/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading;
+
+namespace Carros.Cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(40000);
+            using (var client = new HttpClient())
+            {
+                var result = client.GetAsync("https://localhost:44353/gateway/carros").Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    string json = result.Content.ReadAsStringAsync().Result;
+                    string[] carros = JsonConvert.DeserializeObject<string[]>(json);
+                    MostrarCarros(carros);
+                }
+            }
+        }
+        static void MostrarCarros(string[] carros)
+        {
+            for (int i = 0; i < carros.Length; i++)
+            {
+                Console.WriteLine($"Carro: {carros[i]}");
+            }
+        }
+    }
+}
